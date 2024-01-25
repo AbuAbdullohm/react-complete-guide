2b659f2a0d76312df456395f723a0e78e0dac50e
Muhammadyusuf <mmprogramming3@gmail.com>
2024-01-25 16:30:48 +0500

    new actions added

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -1,33 +1,51 @@
 import "./App.css";
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
-import TapButton from "./components/TapButton.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
+  const [ myBtn, setMyBtn ] = useState("components")
+
+  function handleSelect(selectedBtn) {
+    setMyBtn(selectedBtn)
+  }
+
   return (
     <div className="App">
       <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
-
           <ul>
             <CoreConcepts {...CORE_CONCEPTS[0]} />
             <CoreConcepts {...CORE_CONCEPTS[1]} />
             <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} /> 
           </ul>
         </section>
-
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TapButton>Components</TapButton>
-            <TapButton>JSX</TapButton>
-            <TapButton>Props</TapButton>
-            <TapButton>State</TapButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+
+          <div id="tab-content">
+            <h3>{EXAMPLES[myBtn].title}</h3>
+            <p>{EXAMPLES[myBtn].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[myBtn].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
--- myReactApp/src/data.js
+++ myReactApp/src/data.js
@@ -26,3 +26,53 @@ export const CORE_CONCEPTS = [
     image: MyImg,
   },
 ];
+
+export const EXAMPLES = {
+  components: {
+    title: "Components",
+    description: "Components are the building block of React applications. A component is a self-contained module (HTML + optional CSS + JS) that renders some output.",
+    code: `
+    function Welcome(){
+      return <h1>Hello, world!</h1>;
+    }`,
+  },
+  jsx: {
+    title: "JSX",
+    description:
+      "JSX is a syntax extension to Javascript. It is similar to a template language, but it has full power of JavaScript (e.g. it may output dynamic content).",
+    code: `
+    <div>
+      <h1>Welcome {userName}</h1>
+      <p>Time to learn React</p>
+    </div>
+    `,
+  },
+  props: {
+    title: "Props",
+    description:
+      "Components accept arbitrary inputs called props. They are like a function arguments.",
+    code: `
+    function Welcome(props){
+      return <h1>Hello, {props.name}</h1>;
+    `,
+  },
+  state: {
+    title: "State",
+    description:
+      "State allows React components to changetheir output over time in response to user action, network responses, and anything else.",
+    code: `
+    function Counter(){
+      const [isVisible, setIsVisible] = useState(false)'
+
+      function handleClick(){
+        setIsVisible(true)'
+      }
+
+      return (
+        <div>
+          
+        </div>
+      )
+    `,
+  },
+};
